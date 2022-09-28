65e0535b5c0a1e025cdb5e820bb216f19a0ab721
Wilmer Coyote <wilmercoyote1113@gmail.com>
2022-09-28 08:09:32 -0600

    Implementación del menu myOrder y corrección de solapamiento de menús, tanto desktop como mobile

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const iconMobileMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu')
+const iconCart = document.querySelector('.navbar-shopping-cart');
+const myOrderMenu = document.querySelector('.product-detail');
 
 emailMenu.addEventListener('click', toggleDesktopMenu);
 iconMobileMenu.addEventListener('click', toggleMobileMenu);
+iconCart.addEventListener('click', toggleMyOrderMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
+    const isMyOrderMenuClosed = myOrderMenu.classList.contains('inactive');
+
+    if (!isMyOrderMenuClosed) {
+        myOrderMenu.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMyOrderMenu () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    myOrderMenu.classList.toggle('inactive');
 }
