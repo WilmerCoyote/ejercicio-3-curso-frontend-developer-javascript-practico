3a7fe57466ff785730dc5f2ed59ad874416825c0
Wilmer Coyote <wilmercoyote1113@gmail.com>
2022-10-04 18:36:42 -0600

    Incorporación de la lista de productos y la función para ingresarlos de forma automática por medio de un array

--- main.js
+++ main.js
@@ -4,10 +4,32 @@ const iconMobileMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu')
 const iconCart = document.querySelector('.navbar-shopping-cart');
 const myOrderMenu = document.querySelector('.product-detail');
+const myOrderBackIcon = document.querySelector('.title-container img');
+const cardsContainer = document.querySelector('.cards-container');
 
 emailMenu.addEventListener('click', toggleDesktopMenu);
 iconMobileMenu.addEventListener('click', toggleMobileMenu);
 iconCart.addEventListener('click', toggleMyOrderMenu);
+myOrderBackIcon.addEventListener('click', closeMyOrderMenu);
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Motorcycle',
+    price: 320,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Car',
+    price: 720,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+renderProducts(productList);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
@@ -35,4 +57,70 @@ function toggleMyOrderMenu () {
     }
 
     myOrderMenu.classList.toggle('inactive');
+}
+
+function closeMyOrderMenu () {
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    myOrderMenu.classList.add('inactive');
+}
+
+/*
+<div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+        <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+        </div>
+        <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div>
+*/
+
+function renderProducts (productArray) {
+    for (product of productArray) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        // createElement se utiliza para crear cualquier elemento exitente en HTML.
+
+        const imgProduct = document.createElement('img');
+        imgProduct.setAttribute('src', product.image);
+        // setAtribute se utiliza para agregar cualquier atributo existente a un elemento de HTML.
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price + '.00';
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const figureCart = document.createElement('figure');
+        
+        const imgAddCart = document.createElement('img');
+        imgAddCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        figureCart.appendChild(imgAddCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(figureCart);
+
+        productCard.appendChild(imgProduct);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
 }
